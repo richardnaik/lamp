76517d33bf7fef776965df196ff227c58c2b9d3b
Richard Naik <izzardius@hotmail.com>
2016-10-26 01:06:05 -0500

    Removed apache vhost config and directory

--- recipes/httpd.rb
+++ recipes/httpd.rb
@@ -1,42 +1,8 @@
 # install httpd package
 yum_package 'httpd'
 
-# create apache vhost directories
-directory '/etc/httpd/sites-available' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
-directory '/etc/httpd/sites-enabled' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
-
-# create vhost directory conf file
-template '/etc/httpd/conf.d/vhosts.conf' do
-  source 'vhosts.conf.erb'
-  mode '0755'
-  action :create
-end
-
-# create example vhost file and enable it
-template '/etc/httpd/sites-available/example.net.conf' do
-  source 'example.net.conf.erb'
-  mode '0755'
-  action :create
-end
-
-link '/etc/httpd/sites-enabled/example.net.conf' do
-  to '/etc/httpd/sites-available/example.net.conf'
-  link_type :hard
-end
-
 # create web roots
-directory '/var/www/example' do
+directory '/var/www' do
   owner 'apache'
   group 'apache'
   mode '0755'
